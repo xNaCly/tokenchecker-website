4701833093333818052713084cadaa5eb8e9ab31
xnacly <boomh0815@gmail.com>
2020-08-20 22:07:48 +0200

    format code & add comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,77 @@
 async function checkToken() {
-	document.getElementById("alert-doe").style.display = "none";
-
+	/*
+	get all fields
+	*/
+	var alert = document.getElementById("alert-doe");
+	var list = document.getElementsByClassName("list-group")[0];
+	var id = document.getElementById("id");
+	var tag = document.getElementById("tag");
+	var tel = document.getElementById("phone");
+	var loc = document.getElementById("locale");
+	var email = document.getElementById("email");
+	var ver = document.getElementById("verified");
+	var profile = document.querySelector("#profile");
 	var token = document.getElementsByClassName("output")[0].value;
 
+	/*
+	make everything invisible if new token is submitted
+	*/
+	alert.style.display = "none";
+	list.style.display = "none";
+	profile.style.display = "none";
+	/*
+	fetch @me object from discord:
+
+	{
+			"id": "",
+			"username": "",
+			"avatar": "",
+			"discriminator": "",
+			"public_flags": 256,
+			"flags": 256,
+			"email": null,
+			"verified": false,
+			"locale": "",
+			"nsfw_allowed": true,
+			"mfa_enabled": false,
+			"phone": null
+		}
+	*/
 	let response = await fetch("https://discordapp.com/api/v6/users/@me", {
 		method: "GET",
 		headers: { Authorization: token },
 	});
-
 	response = await response.json();
 
-	var profile = document.querySelector("#profile");
-
+	/*
+	if token is invalid => means if no username returned
+	*/
 	if (!response.username) {
-		return (document.getElementById("alert-doe").style.display = "block");
+		return (alert.style.display = "block");
 	}
 
+	/*
+	check if user has custom profile picture
+	*/
 	if (response.avatar) {
 		profile.src = "https://cdn.discordapp.com/avatars/" + response.id + "/" + response.avatar + ".png?size=128";
 	} else {
 		profile.src = "https://cdn.discordapp.com/embed/avatars/" + (response.discriminator % 5) + ".png?size=128";
 	}
 
-	profile.style.display = "flex";
-
-	var tag = (document.getElementById("tag").textContent = response.username + "#" + response.discriminator);
-
-	var email = document.getElementById("email");
+	/*
+	assign new values to li fields
+	*/
+	tag.textContent = response.username + "#" + response.discriminator;
 	email.textContent = response.email ? response.email : "no email";
-
-	var ver = document.getElementById("verified");
 	ver.textContent = response.verified ? "verified" : "not verified";
-
-	var id = document.getElementById("id");
+	tel.textContent = response.phone ? response.phone : "no phone";
 	id.textContent = response.id;
-
-	var loc = document.getElementById("locale");
 	loc.textContent = response.locale;
 
-	var tel = document.getElementById("phone");
-	tel.textContent = response.phone ? response.phone : "no phone";
-	document.getElementsByClassName("list-group")[0].style.display = "block";
+	/*
+	make list & profile picture visible
+	*/
+	profile.style.display = "flex";
+	list.style.display = "block";
 }
